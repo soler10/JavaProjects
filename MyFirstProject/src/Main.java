93ee5430f80406f13b148a77a4c30cde583059a1
abril <abril@192.168.1.110>
2024-01-15 16:59:55 +0100

    first commit

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("starting from variables");
+		
+		/* Primitive data types:     Reference data type:
+		 * 8 types,                    unlimited: user defined
+		 * store data,                 stores data & address
+		 * hold one value	           can hold more than value
+		 * faster                      slower
+		 */
+		//Primitive:
+		Boolean a = true;
+		int n = 22;
+		char s = 's';
+		//Reference:
+		String x = "Abril";
+		
+		System.out.println(x+" number is "+n);
+		
+		//Scanner class: --> imported at first line
+		
+		
+		
+		
+	}
+
+}
