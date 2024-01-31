c0a9f15c4943200f299c92a8f91c5a2d52fd137f
Abril <abril@192.168.1.110>
2024-01-31 10:14:39 +0100

    olé

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import java.util.Random;
+import java.util.*;
+
 
 public class Main {
 
@@ -125,7 +128,192 @@ public class Main {
 			System.out.println(index);
 		}
 		*/
+		// ***************** Arrays *****************
+		/*
+		String[] cars= {"A", "B", "C"};
+		String[] letters= new String[3];
+		letters[0]="a";
+		letters[1]="b";
+		letters[2]="c";
+		System.out.println(cars[2]);
+		System.out.println(letters[2]);
+		*/
+		// ***************** 2D Arrays *****************
+		/*
+		String[][] a = new String[2][2];
+		a[0][0]="aa";
+		a[0][1]="aa";
+		a[1][0]="aa";
+		a[0][1]="aa";
+		System.out.println(a[0][0]);
+		*/
+		// ***************** string methods *****************
+		/*
+		String name = "Abril";
+		System.out.println(name.equals("abril"));
+		System.out.println(name.equals("Abril"));
+		System.out.println(name.length());
+		System.out.println(name.charAt(0));
+		System.out.println(name.indexOf("b"));
+		*/
+		// ***************** wrapper classes  *****************
+		/*
+		//Primitive faster, reference data type have methods (like stings)
+		//Primitive            Wrapper
+		//boolean              Boolean
+		//char                 Character
+		//int                  Integer ...
+		//Autoboxing: automatic conversion btwn primitive types and their corresponding obj wrapper class
+		Boolean a = true;
+		Character b = '@';
+		Integer c =  123;
+		Double d=3.14;
+		// a. gives you the methods accessible
+		//Unboxing: reverse of autoboxing. Automatic conversion of wrapper class to primitive
+		*/
+		// ***************** AraayList  *****************
+		/*
+		//stores wrapper classes. Elements can be added or removed after compilation phase
+		ArrayList<String> food = new ArrayList<String>();
+		food.add("pizza");
+		food.add("donut");
+		food.add("noodels");
+		
+		food.set(0, "sushi");
+		
+		for (int i=0; i<food.size(); i++) {
+			System.out.println(food.get(i));
+		}
+		
+		//java.util.*
+		System.out.println(food);
+		ArrayList<String> drink = new ArrayList<String>();
+		drink.add("beer");
+		drink.add("coffee");
+		
+		ArrayList<ArrayList<String>> grocery = new ArrayList<ArrayList<String>>();
+		grocery.add(food);
+		grocery.add(drink);
+		System.out.println(grocery);
+		System.out.println(grocery.get(0).get(0));
+		
+		// ***************** foreach loop *****************
+		//less steps but less flexible
+		String[] animals= {"a", "b", "cat"};
+		
+		ArrayList<String> gats = new ArrayList<String>();
+		gats.add("Miquel");
+		gats.add("Linux");
+		gats.add("Maggie");
+		
+		for(String i : animals) {
+			System.out.println(i);
+		}for(String i : gats) {
+			System.out.println(i);
+		}
+		*/
+		// ***************** creating a method outside main *****************
+		/*
+		String name= "Abril";
+		hello(name);  //arguments
+		int years = 27;
+		age(years);
+		int aa = age(years);
+		*/
+		// ***************** overloaded methods *****************
+		/*
+		//methods that share the same name by t have different parameters
+		//       method name + parameters + method signature
+		System.out.println(add(2, 3));
+		System.out.println(add(2, 3, 4));
+		System.out.println(add(2, 3, 4, 5));
+		*/
+		// ***************** printf *****************
+		/*
+		//Control the printing --> format specifier i més coses!!!!
+		System.out.printf("Aixo %c es un format d'string %d",'@', 123);
+		*/
+		// ***************** final declaration *****************
+		final double PI =3.14158; //can not be changed
+		
+		// ***************** printf *****************
+		
+		
+		// ***************** constructors, classes and methods *****************
+		
+		//Car myCar = new Car();
+		Car myCar = new Car("Tesla", 2022, 70000);
+		System.out.println(myCar.make);
+		myCar.drive();
+		myCar.brake();
+		
+		// ***************** global vs local *****************
+		//local: declared inside a method, visible onli to that method
+		//global = declared outside a method, but within class, visible to all parts of the class
+		DiceRoller mydice = new DiceRoller();
+		
+		// ***************** overload constructors *****************
+		//same name parameters but different parameters (unique signature
+		myCar.drive();
+		myCar.drive(3);
+		
+		// ***************** toString method *****************
+		System.out.println(myCar);   //  = address of the memory
+		//when override the toString method in the class car
+		System.out.println(myCar.toString());
+		
+		// ***************** array of objects *****************
+		
+		Car myCar1 = new Car("Tesla", 2022, 70000);
+		Car myCar2 = new Car("WW", 2000, 50000);
+		Car myCar3 = new Car("WMV", 2001, 80000);
+		//Car[] garage = new Car[3];
+		//garage[0]=myCar1;
+		//garage[1]=myCar2;
+		//garage[2]=myCar3;
+		Car[] garage = {myCar1, myCar2, myCar3};
+		System.out.println(garage[2].toString()+"\n");
+		
+		
+		// ***************** static *****************
+		//Static = modifier. A single copy of a variable/method is created and shared. 
+					// the class "owns" the static memeber
+		Friend f1 = new Friend("Spongebob");
+		Friend f2 = new Friend ("Dora");
+		Friend f3 = new Friend ("Patricio");
+		
+		System.out.println(f1.numberOfFriends);
+		System.out.println(Friend.numberOfFriends);
+		
+		System.out.println(f1.numberOfFriends2);
+		
+		//static method
+		Friend.displayFriends();
 		
 		
 	}
-}
+	//overloaded methods
+	static int add(int a, int b) {
+		System.out.println("1st");
+		return a+b;
+	}
+	static int add(int a, int b, int c) {
+		System.out.println("2nd");
+		return a+b+c;
+	}
+	static int add(int a, int b, int c, int d) {
+		System.out.println("3rd");
+		return a+b+c+d;
+	}
+	
+	
+	
+	//crrating a method Static -- bc we call hello method from a static method??
+	static void hello(String name) {
+		System.out.println("Hello "+name);
+	}
+	static int age(int y) {
+		System.out.println("you are "+y+" years old");
+		return y;
+	}
+	}
